407cc1340cabdf478d1522e7f7e078828c80d40e
lross2k <luisross2000@outlook.com>
2024-12-01 21:24:58 -0600

    Initial commit

---
+++ aoc2024/src/main/java/com/lross2k/aoc2024/Aoc2024.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lross2k.aoc2024;
+
+/**
+ *
+ * @author lross2k
+ */
+public class Aoc2024 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello AOC 2024!");
+    }
+}
